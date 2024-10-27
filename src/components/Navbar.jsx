a6f715ffe951ca0ed51ed61697817c2de9f7aa1e
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2024-10-27 20:21:30 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import "./Navbar.css"
 
 const Navbar = () => {
     return (
-        <div className='Navbar1'>
+        <div className='Navbar1 max-sm:block'>
             <span className='font-extrabold  text-xl text-white'>iTask</span>
 
             <ul>
